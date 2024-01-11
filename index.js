1d3118eb4fe3f3a6bb431fc9163ef06cd9abd3c3
Nabz99 <nabil.taleb.dz@viacesi.fr>
2024-01-11 17:23:45 +0100

    maybe last

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ try {
     });
 
 
-  const port = process.env.PORT || 5000;
+  const port = process.env.PORT || 3000;
 
   app.listen(port, () => {
     console.log('Server is listening on port', port);
